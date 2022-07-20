569490611dc566f04efd96e5e1a3a81c73a9f293
ProCode2 <iamprobablycoding@gmail.com>
2022-07-20 19:02:03 +0530

    fix serverless function

--- api/checkScore.js
+++ api/checkScore.js
@@ -1,11 +1,11 @@
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_API_KEY,
-  authDomain: import.meta.env.VITE_AUTH_DOMAIN,
-  projectId: import.meta.env.VITE_PROJECT_ID,
-  storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
-  messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID,
-  appId: import.meta.env.VITE_APP_ID,
-  measurementId: import.meta.env.VITE_MEASUREMENT_ID,
+  apiKey: process.env.VITE_API_KEY,
+  authDomain: process.env.VITE_AUTH_DOMAIN,
+  projectId: process.env.VITE_PROJECT_ID,
+  storageBucket: process.env.VITE_STORAGE_BUCKET,
+  messagingSenderId: process.env.VITE_MESSAGING_SENDER_ID,
+  appId: process.env.VITE_APP_ID,
+  measurementId: process.env.VITE_MEASUREMENT_ID,
 };
 
 import { initializeApp } from "firebase/app";
@@ -23,6 +23,7 @@ const db = getFirestore(app);
 
 export default async function handler(request, response) {
   const { quiz } = request.body;
+  if (!quiz?.quizId) response.status(400).send("quizId is required");
   const answer = await getDoc(
     query(collection(db, "answers"), where("quizId", "==", quiz.quizId))
   );
